1c1cbbcfb91b5cdf28e99f870af017455a71d11e
Roberto <juanrobcode@gmail.com>
2025-03-28 16:36:26 -0700

    some models registered

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Student,Teacher,TakenQuiz,Answer,StudentAnswer
+from .models import Student,Teacher,TakenQuiz,Answer,StudentAnswer,Quiz,Question
 
 
 admin.site.register(Student)
 admin.site.register(Teacher)
+admin.site.register(Quiz)
 admin.site.register(TakenQuiz)
+admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(StudentAnswer)
